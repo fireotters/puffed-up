e6524563d17b462acf1102b98d7547ff7f2218a7
Rioni <nrosa01@ucm.es>
2024-08-18 15:54:08 +0200

    Changed Damager callback from Enter to Stay
    That way, when the fish is invulnerable and gets to the damage area, it will still try to damage it. There are more efficient and fancy solutions but this just works xD

--- Assets/Scripts/GameLogic/Entities/Entity Components/Damager.cs
+++ Assets/Scripts/GameLogic/Entities/Entity Components/Damager.cs
@@ -6,7 +6,7 @@ public class Damager : MonoBehaviour
 {
     [SerializeField] int damage;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.gameObject.TryGetComponent<HealthHandler>(out HealthHandler hh))
         {
