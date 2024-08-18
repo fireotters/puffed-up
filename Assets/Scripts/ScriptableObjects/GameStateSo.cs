5414b4981c6b4024e1acf4efdb2c9baf3465737d
CrossfireCam <30458404+crossfirecam@users.noreply.github.com>
2024-08-18 20:23:38 +1000

    Canvas start
    - Not functional yet

---
+++ Assets/Scripts/ScriptableObjects/GameStateSo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GameLogic
+{
+    [CreateAssetMenu(fileName = "GameStateSo", menuName = "FireOtters/GameStateSo", order = 0)]
+    public class GameStateSo : ScriptableObject
+    {
+
+    }
+}
