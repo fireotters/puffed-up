68c8efce7b8d5494b5c930d15a7db9efa35e0eae
CrossfireCam <30458404+crossfirecam@users.noreply.github.com>
2024-08-19 12:49:17 +1000

    Seaweed prefab
    - Because tiled sprite & collider sizes are separate, collider will resize on start to match the sprite's size
    - Also moved tileset files into folders, big annoying in inspector

--- Assets/Scripts/GameLogic/Entities/Seaweed.cs
+++ Assets/Scripts/GameLogic/Entities/Seaweed.cs
@@ -1,10 +1,19 @@
 using GameLogic;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Seaweed : MonoBehaviour
 {
+    private void Start()
+    {
+        // Resize seaweed's collider to match the sprite size
+        SpriteRenderer _spr = GetComponent<SpriteRenderer>();
+        BoxCollider2D _col = GetComponent<BoxCollider2D>();
+        _col.size = new Vector2(_spr.size.x, _spr.size.y);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.TryGetComponent(out Player player))
