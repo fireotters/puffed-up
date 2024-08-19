69c2a98395154d5e61b6ff320b6de16f05594e69
CrossfireCam <30458404+crossfirecam@users.noreply.github.com>
2024-08-19 17:25:25 +1000

    Razorfish sticking, animator & sounds
    - Razorfish idles & swims. I found no use for Bonk anim?
    - Put in 'attack' and 'stick' sounds
    - When the razorfish sticks, it'll disable all Rigidbody movement & rotation
    - Razorfish body is on a new 'Enemies_DontCareAboutWalls' physics layer - they can be placed in the floor & only have their tip poking out

--- Assets/Scripts/GameLogic/Entities/Enemies/SwordFishEnemy.cs
+++ Assets/Scripts/GameLogic/Entities/Enemies/SwordFishEnemy.cs
@@ -1,6 +1,8 @@
 using System;
 using ExtensionsFunctions;
+using FMODUnity;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace GameLogic.Entities.Enemies
 {
@@ -11,13 +13,16 @@ namespace GameLogic.Entities.Enemies
         [SerializeField] private LayerMask wallLayer, targetLayerMask;
         private Rigidbody2D _rigidbody2D;
         private bool _charging;
-        
+        [SerializeField] private StudioEventEmitter sndRazorAttack, sndRazorStick;
+        private bool alreadyPlayedAttackSnd = false;
+        private Animator _animator;
+
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
-        // Update is called once per frame
         private void Update()
         {
             var hit = Physics2D.Raycast(swordTip.transform.position, transform.up, Mathf.Infinity, targetLayerMask);
@@ -25,19 +30,35 @@ namespace GameLogic.Entities.Enemies
             if (hit.collider != null && hit.collider.gameObject.GetComponent<Player>())
             {
                 // i saw the player!!!!!!! get its shit
-                print("SAW YOU");
+                // print("SAW YOU");
                 _charging = true;
+                if (!alreadyPlayedAttackSnd)
+                {
+                    int swimType = (int)Math.Round(Random.Range(1f, 2f));
+                    _animator.Play("Swim" + swimType);
+                    sndRazorAttack.Play();
+                    alreadyPlayedAttackSnd = true;
+                }
             }
-            
             if (_charging)
+            {
+                _animator.SetFloat("speed", _rigidbody2D.velocity.magnitude / 7);
                 _rigidbody2D.AddForce(transform.up * moveSpeed);
+            }
         }
 
-        private void OnTriggerEnter2D(Collider2D other)
+        private void OnCollisionEnter2D(Collision2D col)
         {
-            if (other.gameObject.IsInLayerMask(wallLayer))
+            // If tip touches a wall, play Stick sfx & force enemy to stay still
+            if (col.otherCollider.gameObject == swordTip)
             {
-                print("tip touched wall!");    
+                if (col.gameObject.IsInLayerMask(wallLayer))
+                {
+                    sndRazorStick.Play();
+                    _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+                    _animator.Play("Idle");
+                    // print("tip touched wall!");
+                }
             }
         }
     }
