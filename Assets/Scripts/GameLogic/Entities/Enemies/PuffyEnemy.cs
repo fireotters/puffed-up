9ba1d0602722bd2a00720fa340f7dc261ed8d574
TeslaSP2 <soloparainformatica@gmail.com>
2024-08-19 21:45:42 +0200

    puffy enemy animations

--- Assets/Scripts/GameLogic/Entities/Enemies/PuffyEnemy.cs
+++ Assets/Scripts/GameLogic/Entities/Enemies/PuffyEnemy.cs
@@ -6,16 +6,19 @@ namespace GameLogic.Entities.Enemies
     public class PuffyEnemy : MonoBehaviour
     {
         private PuffStateHandler _enemyPuffStateHandler;
+        private Animator _animator;
 
         private void Start()
         {
             _enemyPuffStateHandler = GetComponent<PuffStateHandler>();
+            _animator = GetComponent<Animator>();
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.GetComponent<Player>())
             {
+                _animator.Play("Scare");
                 _enemyPuffStateHandler.SetPuffed();
             }
         }
