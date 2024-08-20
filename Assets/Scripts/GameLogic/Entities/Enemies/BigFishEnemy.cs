d54e280a627e4a29b39e9c67b693fa5e6e4be122
CrossfireCam <30458404+crossfirecam@users.noreply.github.com>
2024-08-20 19:43:27 +1000

    BigFish: Animator, sfx
    - Flips depending on direction
    - Swims, scares, idles
    - Sfx when touching non-player object, sfx when chasing or giving up chasing player

--- Assets/Scripts/GameLogic/Entities/Enemies/BigFishEnemy.cs
+++ Assets/Scripts/GameLogic/Entities/Enemies/BigFishEnemy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using ExtensionsFunctions;
+using FMODUnity;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -16,27 +17,39 @@ namespace GameLogic.Entities.Enemies
         private CancellationTokenSource _cancellationToken = new();
         private Animator _animator;
 
+        // Sound
+        [SerializeField] private StudioEventEmitter sndEnemyTriggered, sndEnemyBounce;
+        private float checkTimeBounceSfx, delayBetweenBounceSfx = 1f;
+
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
+            checkTimeBounceSfx = Time.time + delayBetweenBounceSfx;
         }
-        
+
         private void Update()
         {
             if (!_standby) return;
-            
+
+            Vector2 direction = new Vector2();
             if (_chasing)
             {
                 //chasing behaviour
-                var direction = _target - (Vector2) transform.position;
+                direction = _target - (Vector2)transform.position;
                 _rigidbody2D.AddForce(direction.normalized * moveSpeed);
+                _animator.SetFloat("speed", _rigidbody2D.velocity.magnitude / 7);
+                int swimType = (int)Math.Round(Random.Range(1f, 2f));
+                _animator.Play("Swim" + swimType);
             }
             else if (_evading)
             {
                 // evading behaviour
-                var direction = _target + (Vector2) transform.position;
+                direction = _target + (Vector2)transform.position;
                 _rigidbody2D.AddForce(direction.normalized * moveSpeed);
+                _animator.SetFloat("speed", _rigidbody2D.velocity.magnitude / 7);
+                int swimType = (int)Math.Round(Random.Range(1f, 2f));
+                _animator.Play("Scare" + swimType);
             }
             else
             {
@@ -46,7 +59,7 @@ namespace GameLogic.Entities.Enemies
                 var duration = Random.Range(0f, 4f);
                 if (Random.Range(0, 2) == 1)
                 {
-                    var direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+                    direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
 
                     //print($"Imma go {direction} for {Random.Range(0f, 4f)}s!");
                     _normalBehaviourRunning = true;
@@ -58,7 +71,8 @@ namespace GameLogic.Entities.Enemies
                 }
                 else
                 {
-                    print("I aint movin");
+                    //print("I aint movin");
+                    _animator.Play("Idle");
                     _normalBehaviourRunning = true;
                     this.ExecuteOverDuration(duration, _cancellationToken.Token, time =>
                     {
@@ -66,6 +80,15 @@ namespace GameLogic.Entities.Enemies
                         if (time == 1f) _normalBehaviourRunning = false;
                     }).Forget();
                 }
+            }
+            // Rotate sprite
+            if (direction.x > 0f)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+            }
+            else if (direction.x < -0f)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
             }
         }
 
@@ -85,11 +108,23 @@ namespace GameLogic.Entities.Enemies
         {
             GenericExtensions.CancelAndGenerateNew(ref _cancellationToken);
             _normalBehaviourRunning = false;
+            if (other.gameObject.TryGetComponent(out Player player) == false)
+            {
+                if (Time.time > checkTimeBounceSfx)
+                {
+                    checkTimeBounceSfx = Time.time + delayBetweenBounceSfx;
+                    sndEnemyBounce.Play();
+                }
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             FindPlayerAndSetEnemyState(other);
+            if (other.gameObject.TryGetComponent(out Player player))
+            {
+                sndEnemyTriggered.Play();
+            }
         }
 
         private void OnTriggerStay2D(Collider2D other)
@@ -102,6 +137,10 @@ namespace GameLogic.Entities.Enemies
             // print("Stopping chase");
             _evading = false;
             _chasing = false;
+            if (other.gameObject.TryGetComponent(out Player player))
+            {
+                sndEnemyTriggered.Play();
+            }
         }
 
         public void SetBehaviour(bool enabled)
