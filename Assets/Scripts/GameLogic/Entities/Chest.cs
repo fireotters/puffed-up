d0fa9c4680e4847bd847ab510da31bd0895be74c
CrossfireCam <30458404+crossfirecam@users.noreply.github.com>
2024-08-20 17:18:36 +1000

    Chest extra prefabs & fixes
    - Added two prefab variants that change the particle systems & effector height
    - Will only target Player layer. Enemies are no longer targeted (if you'd like to bring this back, put Razorfish & Hand onto a unique Enemy layer?)
    - Waits 1sec until enabling the Area Effector, gives the player time to react to a chest opening

--- Assets/Scripts/GameLogic/Entities/Chest.cs
+++ Assets/Scripts/GameLogic/Entities/Chest.cs
@@ -16,6 +16,11 @@ namespace GameLogic
         public void EnableChest()
         {
             _box.sprite = openSprite;
+            Invoke(nameof(DelayAreaEffector), 1f); // Bubbles don't really start appearing until about 1s into particles being activated. Let's delay the effector
+        }
+
+        private void DelayAreaEffector()
+        {
             areaEffector2D.enabled = true;
         }
 
